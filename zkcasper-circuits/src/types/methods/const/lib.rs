df58936358195b0a9329dc2c9eaceac9f87bee7f
Daniel Choi <choidanielw@gmail.com>
2023-06-29 15:00:26 -0700

    wip: update values

--- zkcasper-circuits/src/types/methods/const/lib.rs
+++ zkcasper-circuits/src/types/methods/const/lib.rs
@@ -1,33 +1,22 @@
+import { 
+    BeaconState
+} from "@lodestar/types/phase0"
+
 use typenum::{
-    U1, U10, U100, U31, U32, U5, U8,
+    U1, U10, U100, U31, U41, U43, U5, U8, U9,
 };
 
 pub trait Spec: 'static + Default + Debug {
     type MaxValidators: Unsigned + Clone + Sync + Send + Debug + PartialEq;
     type Validator0GIndex: Unsigned + Clone + Sync + Send + Debug + PartialEq;
-    type MaxNBytesInteger: Unsigned + Clone + Sync + Send + Debug + PartialEq;
-    type NBytesU64: Unsigned + Clone + Sync + Send + Debug + PartialEq;
     type ChunksPerValidator: Unsigned + Clone + Sync + Send + Debug + PartialEq;
     type UsedChunksPerValidator: Unsigned + Clone + Sync + Send + Debug + PartialEq;
     type TreeDepth: Unsigned + Clone + Sync + Send + Debug + PartialEq;
-    type TreeLevelAuxColumn: Unsigned + Clone + Sync + Send + Debug + PartialEq;
     type PubkeysLevel: Unsigned + Clone + Sync + Send + Debug + PartialEq;
     // type ValidatorsLevel: Unsigned + Clone + Sync + Send + Debug + PartialEq;
 
     fn max_validators() -> usize {
         Self::MaxValidators::to_usize()
-    }
-
-    fn validator0_gindex() -> usize {
-        Self::Validator0GIndex::to_usize()
-    }
-
-    fn max_n_bytes_integer() -> usize {
-        Self::MaxNBytesInteger::to_usize()
-    }
-
-    fn n_bytes_u64() -> usize {
-        Self::NBytesU64::to_usize()
     }
 
     fn chunks_per_validator() -> usize {
@@ -42,12 +31,8 @@ pub trait Spec: 'static + Default + Debug {
         Self::TreeDepth::to_usize()
     }
 
-    fn tree_level_aux_column() -> usize {
-        Self::TreeLevelAuxColumn::to_usize()
-    }
-
     fn pubkeys_level() -> usize {
-        Self::PubkeysLevel::to_usize()
+        BeaconState.getPathInfo(["validators", 0, "pubkey"]) * 2n;
     }
 
     fn validators_level() -> usize {
@@ -65,13 +50,23 @@ pub trait Spec: 'static + Default + Debug {
 pub struct Test;
 
 impl Spec for Test {
-    type MaxValidators = U32;
+    type MaxValidators = U100;
     type Validator0GIndex = U100;
-    type MaxNBytesInteger = U31;
-    type NBytesU64 = U8;
     type ChunksPerValidator = U8;
     type UsedChunksPerValidator = U5;
     type TreeDepth = U10;
-    type TreeLevelAuxColumn = U1;
+    type PubkeysLevel = U10;
+}
+
+pub struct Mainnet;
+
+impl Spec for Mainnet {
+    // (2**40)=1099511627776
+    type MaxValidators = Prod<EXP<U2>, U40>;
+    // 94557999988736
+    type Validator0GIndex = Prod<Exp<U2, U41>, U43>;
+    type ChunksPerValidator = U9;
+    type UsedChunksPerValidator = U5;
+    type TreeDepth = U10;
     type PubkeysLevel = U10;
 }
