67a0962cc48dfa4398766af823b94fdfc4702a3e
Daniel Choi <choidanielw@gmail.com>
2023-08-15 15:50:56 -0700

    edits + include committee aggpubkeys

--- preprocessor/scripts/beaconState.ts
+++ preprocessor/scripts/beaconState.ts
@@ -1,48 +1,76 @@
 import fs from "fs";
-import {getClient, HttpError} from "@lodestar/api";
+import {getClient } from "@lodestar/api";
 import {config} from "@lodestar/config/default";
-import {ssz} from "@lodestar/types";
+import {
+    ssz, 
+    BLSPubkey, 
+    ValidatorIndex
+} from "@lodestar/types";
+import {fromHex} from "@lodestar/utils"
+import {BeaconStateAllForks } from "@lodestar/state-transition";
 import {
     ContainerType,
-    ContainerNodeStructType,
     ListCompositeType,
     ValueOf
 } from "@chainsafe/ssz";
-import { serialize } from "./util";
-import { createProof, ProofType, MultiProof, Node, Gindex} from "@chainsafe/persistent-merkle-tree";
-import { createNodeFromMultiProofWithTrace, printTrace } from "./merkleTrace";
-import {fromHex} from "@lodestar/utils"
+import { createProof, ProofType, MultiProof } from "@chainsafe/persistent-merkle-tree";
+import { bls12_381 } from '@noble/curves/bls12-381';
+import { bytesToHex } from "@noble/curves/abstract/utils";
+import { ProjPointType } from "@noble/curves/abstract/weierstrass";
+import { g1PointToLeBytes as g1PointToBytesLE, serialize, chunkArray } from "./util";
+import { createNodeFromMultiProofWithTrace } from "./merkleTrace";
+
+// convenience functions for operations for beacon state collection and parsing
+// the below function is pulled from lodestar/ 
+export function getPubkeysForIndices(
+    validators: BeaconStateAllForks["validators"],
+    indexes: ValidatorIndex[]
+  ): BLSPubkey[] {
+    const validatorsLen = validators.length; // Get once, it's expensive
+  
+    const pubkeys: BLSPubkey[] = [];
+    for (let i = 0, len = indexes.length; i < len; i++) {
+      const index = indexes[i];
+      if (index >= validatorsLen) {
+        throw Error(`validatorIndex ${index} too high. Current validator count ${validatorsLen}`);
+      }
+  
+      // NOTE: This could be optimized further by traversing the tree optimally with .getNodes()
+      const validator = validators.getReadonly(index);
+      pubkeys.push(validator.pubkey);
+    }
+  
+    return pubkeys;
+  }
+
 
 
 // Testing Constants
-    // commenting out in favor of a flexible validators count ==> set to beaconstate.validators.length
 const N_VALIDATORS_COUNT = 100;
-const DST = "BLS_SIG_BLS12381G2_XMD:SHA-256_SSWU_RO_POP_";
-
 const VALIDATOR_LIMIT = 1099511627776;
-const TARGET_EPOCH = 90625;
-const SLOTS_IN_EPOCH = [2900001, 2900032]
 
 // sepolia endpoint
 // const api = getClient({baseUrl: "https://lodestar-sepolia.chainsafe.io"}, {config});
 // sepolia beacon node endpoint
 const api = getClient({baseUrl: "http://3.133.148.86:80"}, {config});
 
-const beaconstateApi = await 
-    api.debug
+async function beaconstateApi(slot: string) {
+    return await api.debug
     .getStateV2(
-        "head",
-        "ssz")
+            slot,
+            "ssz"
+        )
     .then((res) => {
         if (res.ok) {
             return res.response;
         } else {
-            console.error(res.status, res.error.code, res.error.message);
+            // console.error(res.status, res.error.code, res.error.message);
             return new Uint8Array;
         }
     });
+}
 
-let beaconstateSsz = beaconstateApi;
+let beaconstateSsz = await beaconstateApi("head");
 let beaconstateJson = ssz.capella.BeaconState.toJson(ssz.capella.BeaconState.deserialize(beaconstateSsz));
 let beaconstateDeserialized = ssz.capella.BeaconState.deserializeToViewDU(beaconstateSsz);
 
@@ -72,16 +100,7 @@ const ValidatorContainer = new ContainerType(
     }
 );
 
-// mainnet beacon state validator limit = 1099511627776
-// must use this value to have the proper tree depth and and chunk depth
-// treeDepth == 41
-// chunkDepth == 40
-// let n_validators_count = beaconstateJson.validators.length;
 export const ValidatorsSsz = new ListCompositeType(ValidatorContainer, VALIDATOR_LIMIT);
-
-// mainnet validators length: 853056
-// sepolia validators length:   1973
-// console.log("beaconstate ssz validators length: ", beaconstateJson.validators.length);
 
 // for loop in which the constant set for N_VALIDATORS_COUNT
 // size of this array will be set by constant
@@ -118,7 +137,6 @@ for (let i=0; i<N_VALIDATORS_COUNT-1;i++) {
 
 let view = ValidatorsSsz.toView(validators);
 
-
 //----------------- State tree -----------------//
 
 let proof = createProof(view.node, { type: ProofType.multi, gindices: gindices }) as MultiProof;
@@ -128,7 +146,7 @@ const areEqual = (first: Uint8Array, second: Uint8Array) =>
 
 let [partial_tree, trace] = createNodeFromMultiProofWithTrace(proof.leaves, proof.witnesses, proof.gindices, nonRlcGindices);
 
-printTrace(partial_tree, trace);
+// printTrace(partial_tree, trace);
 
 // console.log("proof: ", proof)
 // console.log("trace: ", serialize(trace))
@@ -138,6 +156,67 @@ fs.writeFileSync(
     serialize(trace)
 )
 
+//----------------- Committees -----------------//
+
+const getBlock = await 
+    api.beacon
+    .getBlock(
+        "head")
+    .then((res) => {
+        if (res.ok) {
+            return res.response;
+        } else {
+            console.error(res.status, res.error.code, res.error.message);
+            return new Uint8Array;
+        }
+    });
+
+let headBlock = getBlock;
+// get current head slot and corresponding epochs
+let headSlot = headBlock.data.message.slot
+let headEpoch = headBlock.data.message.body.attestations[0].data.target.epoch
+let targetEpoch = headEpoch-1;
+
+let pubKeyPoints: ProjPointType<bigint>[] = [];
+
+async function getEpochCommitteeApi(stateId: number, epoch?: number) {
+    return await api.beacon
+    .getEpochCommittees(
+            stateId,
+            {"epoch": epoch}
+        )
+    .then((res) => {
+        if (res.ok) {
+            return res.response;
+        } else {
+            // console.error(res.status, res.error.code, res.error.message);
+            return undefined;
+        }
+    });
+}
+
+// passing epoch number precedes the slot parameter.
+// getEpochCommittees includes validator shuffling
+let committee = await getEpochCommitteeApi(headSlot, targetEpoch);
+
+for (i=0;i<committee.data.length;i++) {
+    // gets all pubkeys of validators in committee.data.validators array as a Point.
+    pubKeyPoints.push(getPubkeysForIndices(beaconstateDeserialized.validators, committee.data[i].validators).map(bytes => bls12_381.G1.ProjectivePoint.fromHex(bytesToHex(bytes))));
+}
+
+pubKeyPoints = pubKeyPoints.flat(1);
+const committeePubkeys = chunkArray(pubKeyPoints, pubKeyPoints.length);
+const aggregatedPubKeys = committeePubkeys.map((pubKeys) => bls12_381.aggregatePublicKeys(pubKeys));
+let bytesPubkeys = aggregatedPubKeys.map((aggPubkey) => Array.from(g1PointToBytesLE(aggPubkey, false)));
+
+console.log("bytesPubkeys: ", bytesPubkeys);
+
+console.log("serialize bytespubkeys: ", serialize(bytesPubkeys));
+
+fs.writeFileSync(
+    `../test_data/sepolia_aggregated_pubkeys.json`,
+    serialize(bytesPubkeys)
+);
 
 // ------------ Attestations ------------ //
 
@@ -159,14 +238,15 @@ async function getBlockAttestationApi(slot: number) {
     });
 }
 
-// get all blocks within a target epoch
-for (var i=SLOTS_IN_EPOCH[0]; i<=SLOTS_IN_EPOCH[1]; i++) {
+// get all blocks within a target epoch with known slot id's for corresponding epoch.
+for (var i=headSlot; ; i--) {
     let blockAttestation = await getBlockAttestationApi(i);
     if (blockAttestation == undefined) {
         // console.log("empty slot");
         continue;
     };
-    if (blockAttestation.data[0].data.target.epoch == TARGET_EPOCH) {
+    // set headEpoch -1 to have TARGET_EPOCH = head target epoch - 1.
+    if (blockAttestation.data[0].data.target.epoch == targetEpoch) {
         let data = {
             slot: blockAttestation.data[0].data.slot,
             index: blockAttestation.data[0].data.index,
@@ -186,21 +266,21 @@ for (var i=SLOTS_IN_EPOCH[0]; i<=SLOTS_IN_EPOCH[1]; i++) {
             data: data,
             signature: blockAttestation.data[0].signature
         });
+    }
 
+    // keeps running the loop until the source epoch == target epoch -2; which will confirm that the loop has run for the entire target epoch.
+    if (blockAttestation.data[0].data.source.epoch == targetEpoch-2) {
+        break;
     }
 
 };
 
 let attestationJson = ssz.phase0.BeaconBlockBody.fields.attestations.toJson(attestations);
 
-// console.log(attestationJson);
+// console.log("attestations: ", attestationJson);
 
 fs.writeFileSync(
     `../test_data/sepolia_attestations.json`,
     JSON.stringify(attestationJson)
 );
-
-
-
-
 
