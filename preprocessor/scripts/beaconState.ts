5d9be63cc72d2f96162a09f91b4d68e4272bd4c3
Daniel Choi <choidanielw@gmail.com>
2023-08-22 18:19:06 -0700

    include validators file generation

--- preprocessor/scripts/beaconState.ts
+++ preprocessor/scripts/beaconState.ts
@@ -20,6 +20,14 @@ import { ProjPointType } from "@noble/curves/abstract/weierstrass";
 import { g1PointToLeBytes as g1PointToBytesLE, serialize, chunkArray } from "./util";
 import { createNodeFromMultiProofWithTrace } from "./merkleTrace";
 
+
+// Testing Constants
+const N_VALIDATORS_COUNT = 100;
+const VALIDATOR_LIMIT = 1099511627776;
+
+const N_VALIDATORS = parseInt(process.argv[2]) || 5;
+const N_COMMITTEES = parseInt(process.argv[3]) || 1;
+
 // convenience functions for operations for beacon state collection and parsing
 // the below function is pulled from lodestar/ 
 export function getPubkeysForIndices(
@@ -43,12 +51,6 @@ export function getPubkeysForIndices(
     return pubkeys;
   }
 
-
-
-// Testing Constants
-const N_VALIDATORS_COUNT = 100;
-const VALIDATOR_LIMIT = 1099511627776;
-
 // sepolia endpoint
 // const api = getClient({baseUrl: "https://lodestar-sepolia.chainsafe.io"}, {config});
 // sepolia beacon node endpoint
@@ -71,90 +73,9 @@ async function beaconstateApi(slot: string) {
 }
 
 let beaconstateSsz = await beaconstateApi("head");
-let beaconstateJson = ssz.capella.BeaconState.toJson(ssz.capella.BeaconState.deserialize(beaconstateSsz));
+let beaconstate = ssz.capella.BeaconState.deserialize(beaconstateSsz);
+let beaconstateJson = ssz.capella.BeaconState.toJson(beaconstate);
 let beaconstateDeserialized = ssz.capella.BeaconState.deserializeToViewDU(beaconstateSsz);
-
-// ------------ Validators Data ------------ // 
-
-type Validator = ValueOf<typeof ValidatorContainer>;
-let validators: Validator[] = [];
-let validatorBaseGindices: bigint[] = [];
-let gindices: bigint[] = [];
-// the gindices of the fields that are <= 31 bytes
-let nonRlcGindices = [];
-
-const ValidatorContainer = new ContainerType(
-    {
-        pubkey: ssz.Bytes48,
-        withdrawalCredentials: ssz.Bytes32,
-        effectiveBalance: ssz.UintNum64,
-        slashed: ssz.Boolean,
-        activationEligibilityEpoch: ssz.EpochInf,
-        activationEpoch: ssz.EpochInf,
-        exitEpoch: ssz.EpochInf,
-        withdrawableEpoch: ssz.EpochInf,
-    },
-    { 
-        typeName: "Validator",
-        jsonCase: "eth2" 
-    }
-);
-
-export const ValidatorsSsz = new ListCompositeType(ValidatorContainer, VALIDATOR_LIMIT);
-
-// for loop in which the constant set for N_VALIDATORS_COUNT
-// size of this array will be set by constant
-for (let i=0; i<N_VALIDATORS_COUNT-1;i++) {
-    let pubkey = fromHex(beaconstateJson.validators[i].pubkey);
-    const paddedPubkey = new Uint8Array(48);
-    paddedPubkey.set(pubkey, 0);
-
-    validators.push({
-        pubkey: paddedPubkey,
-        withdrawalCredentials: fromHex(beaconstateJson.validators[i].withdrawal_credentials),
-        effectiveBalance: beaconstateJson.validators[i].effective_balance,
-        slashed: beaconstateJson.validators[i].slashed,
-        activationEligibilityEpoch: beaconstateJson.validators[i].activation_eligibility_epoch,
-        activationEpoch: beaconstateJson.validators[i].activation_epoch,
-        exitEpoch: beaconstateJson.validators[i].exit_epoch,
-        withdrawableEpoch: beaconstateJson.validators[i].withdrawable_epoch
-    });
-
-    validatorBaseGindices.push(ValidatorsSsz.getPathInfo([i]).gindex);
-    gindices.push(ValidatorsSsz.getPathInfo([i, 'pubkey']).gindex * 2n);
-    gindices.push(ValidatorsSsz.getPathInfo([i, 'pubkey']).gindex * 2n + 1n);
-    gindices.push(ValidatorsSsz.getPathInfo([i, 'effectiveBalance']).gindex);
-    gindices.push(ValidatorsSsz.getPathInfo([i, 'slashed']).gindex);
-    gindices.push(ValidatorsSsz.getPathInfo([i, 'activationEpoch']).gindex);
-    gindices.push(ValidatorsSsz.getPathInfo([i, 'exitEpoch']).gindex);
-
-    nonRlcGindices.push(ValidatorsSsz.getPathInfo([i, 'effectiveBalance']).gindex);
-    nonRlcGindices.push(ValidatorsSsz.getPathInfo([i, 'slashed']).gindex);
-    nonRlcGindices.push(ValidatorsSsz.getPathInfo([i, 'activationEpoch']).gindex);
-    nonRlcGindices.push(ValidatorsSsz.getPathInfo([i, 'exitEpoch']).gindex);
-
-}
-
-let view = ValidatorsSsz.toView(validators);
-
-//----------------- State tree -----------------//
-
-let proof = createProof(view.node, { type: ProofType.multi, gindices: gindices }) as MultiProof;
-
-const areEqual = (first: Uint8Array, second: Uint8Array) =>
-    first.length === second.length && first.every((value, index) => value === second[index]);
-
-let [partial_tree, trace] = createNodeFromMultiProofWithTrace(proof.leaves, proof.witnesses, proof.gindices, nonRlcGindices);
-
-// printTrace(partial_tree, trace);
-
-// console.log("proof: ", proof)
-// console.log("trace: ", serialize(trace))
-
-fs.writeFileSync(
-    `../test_data/sepolia_beacon_state.json`,
-    serialize(trace)
-)
 
 //----------------- Committees -----------------//
 
@@ -204,19 +125,119 @@ for (i=0;i<committee.data.length;i++) {
     pubKeyPoints.push(getPubkeysForIndices(beaconstateDeserialized.validators, committee.data[i].validators).map(bytes => bls12_381.G1.ProjectivePoint.fromHex(bytesToHex(bytes))));
 }
 
+// ------------ Pubkeys ------------ //
+
 pubKeyPoints = pubKeyPoints.flat(1);
 const committeePubkeys = chunkArray(pubKeyPoints, pubKeyPoints.length);
 const aggregatedPubKeys = committeePubkeys.map((pubKeys) => bls12_381.aggregatePublicKeys(pubKeys));
 let bytesPubkeys = aggregatedPubKeys.map((aggPubkey) => Array.from(g1PointToBytesLE(aggPubkey, false)));
 
-console.log("bytesPubkeys: ", bytesPubkeys);
-
-console.log("serialize bytespubkeys: ", serialize(bytesPubkeys));
+// console.log("bytesPubkeys: ", bytesPubkeys);
+// console.log("serialize bytespubkeys: ", serialize(bytesPubkeys));
 
 fs.writeFileSync(
     `../test_data/sepolia_aggregated_pubkeys.json`,
     serialize(bytesPubkeys)
 );
+
+// ------------ Validators ------------ // 
+
+type Validator = ValueOf<typeof ValidatorContainer>;
+let validators: Validator[] = [];
+let validatorBaseGindices: bigint[] = [];
+let gindices: bigint[] = [];
+// the gindices of the fields that are <= 31 bytes
+let nonRlcGindices = [];
+
+const ValidatorContainer = new ContainerType(
+    {
+        pubkey: ssz.Bytes48,
+        withdrawalCredentials: ssz.Bytes32,
+        effectiveBalance: ssz.UintNum64,
+        slashed: ssz.Boolean,
+        activationEligibilityEpoch: ssz.EpochInf,
+        activationEpoch: ssz.EpochInf,
+        exitEpoch: ssz.EpochInf,
+        withdrawableEpoch: ssz.EpochInf,
+    },
+    { 
+        typeName: "Validator",
+        jsonCase: "eth2" 
+    }
+);
+
+export const ValidatorsSsz = new ListCompositeType(ValidatorContainer, VALIDATOR_LIMIT);
+
+// for loop in which the size is N_VALIDATORS * N_COMMITTEES
+for (let i=0; i<N_VALIDATORS * N_COMMITTEES;i++) {
+    let pubkey = fromHex(beaconstateJson.validators[i].pubkey);
+    const paddedPubkey = new Uint8Array(48);
+    paddedPubkey.set(pubkey, 0);
+
+    validators.push({
+        pubkey: paddedPubkey,
+        withdrawalCredentials: fromHex(beaconstateJson.validators[i].withdrawal_credentials),
+        effectiveBalance: beaconstateJson.validators[i].effective_balance,
+        slashed: beaconstateJson.validators[i].slashed,
+        activationEligibilityEpoch: beaconstateJson.validators[i].activation_eligibility_epoch,
+        activationEpoch: beaconstateJson.validators[i].activation_epoch,
+        exitEpoch: beaconstateJson.validators[i].exit_epoch,
+        withdrawableEpoch: beaconstateJson.validators[i].withdrawable_epoch
+    });
+
+    validatorBaseGindices.push(ValidatorsSsz.getPathInfo([i]).gindex);
+    gindices.push(ValidatorsSsz.getPathInfo([i, 'pubkey']).gindex * 2n);
+    gindices.push(ValidatorsSsz.getPathInfo([i, 'pubkey']).gindex * 2n + 1n);
+    gindices.push(ValidatorsSsz.getPathInfo([i, 'effectiveBalance']).gindex);
+    gindices.push(ValidatorsSsz.getPathInfo([i, 'slashed']).gindex);
+    gindices.push(ValidatorsSsz.getPathInfo([i, 'activationEpoch']).gindex);
+    gindices.push(ValidatorsSsz.getPathInfo([i, 'exitEpoch']).gindex);
+
+    nonRlcGindices.push(ValidatorsSsz.getPathInfo([i, 'effectiveBalance']).gindex);
+    nonRlcGindices.push(ValidatorsSsz.getPathInfo([i, 'slashed']).gindex);
+    nonRlcGindices.push(ValidatorsSsz.getPathInfo([i, 'activationEpoch']).gindex);
+    nonRlcGindices.push(ValidatorsSsz.getPathInfo([i, 'exitEpoch']).gindex);
+
+}
+
+let view = ValidatorsSsz.toView(validators);
+
+fs.writeFileSync(
+    `../test_data/sepolia_validators.json`,
+    serialize(Array.from(validators.entries()).map(([i, v]) => ({
+        id: i,
+        shufflePos: i,
+        committee: Math.floor(i / N_VALIDATORS),
+        isActive: !v.slashed && v.activationEpoch <= targetEpoch && targetEpoch < v.exitEpoch,
+        isAttested: true,
+        pubkey: Array.from(v.pubkey),
+        pubkeyUncompressed: Array.from(g1PointToBytesLE(pubKeyPoints[i], false)),
+        effectiveBalance: v.effectiveBalance,
+        slashed: v.slashed,
+        activationEpoch: v.activationEpoch,
+        exitEpoch: v.exitEpoch,
+        gindex: validatorBaseGindices[i]
+    })))
+);
+
+//----------------- State tree -----------------//
+
+let proof = createProof(view.node, { type: ProofType.multi, gindices: gindices }) as MultiProof;
+
+const areEqual = (first: Uint8Array, second: Uint8Array) =>
+    first.length === second.length && first.every((value, index) => value === second[index]);
+
+let [partial_tree, trace] = createNodeFromMultiProofWithTrace(proof.leaves, proof.witnesses, proof.gindices, nonRlcGindices);
+
+// printTrace(partial_tree, trace);
+
+// console.log("proof: ", proof)
+// console.log("trace: ", serialize(trace))
+
+fs.writeFileSync(
+    `../test_data/sepolia_beacon_state.json`,
+    serialize(trace)
+)
 
 // ------------ Attestations ------------ //
 
@@ -283,4 +304,3 @@ fs.writeFileSync(
     `../test_data/sepolia_attestations.json`,
     JSON.stringify(attestationJson)
 );
-
