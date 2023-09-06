b8f60d25cb9b50245077ea6b931590e7b9ad43d6
Daniel Choi <choidanielw@gmail.com>
2023-09-06 15:51:38 -0700

    edit preprocessor script

--- preprocessor/scripts/beaconState.ts
+++ preprocessor/scripts/beaconState.ts
@@ -20,18 +20,23 @@ import { ProjPointType } from "@noble/curves/abstract/weierstrass";
 import { g1PointToLeBytes as g1PointToBytesLE, serialize, chunkArray } from "./util";
 import { createNodeFromMultiProofWithTrace } from "./merkleTrace";
 
-
 // Testing Constants
-const N_VALIDATORS = parseInt(process.argv[2]) || 5;
-const N_COMMITTEES = parseInt(process.argv[3]) || 1;
+// committees will be specified and this will gather the relevant information using committee as the limiter
+let N_COMMITTEES = parseInt(process.argv[2]) || 1;
+
+// File to generate
+const FILE = process.argv[3] || "all";
+// following options are:
+// - pubkeys
+// - aggregation
+// - attestations
+// - trace
 
 // VALIDATOR_LIMIT value is the one that is being used in mainnet/sepolia. This value will set the validator depth of 41n in the merkle tree. 
 const VALIDATOR_LIMIT = 1099511627776;
 
 // local node endpoint
-const NODE_ENDPOINT = "http://3.133.148.86:80";
-// const NODE_ENDPOINT = "https://lodestar-sepolia.chainsafe.io";
-
+const NODE_ENDPOINT = "https://lodestar-sepolia.chainsafe.io";
 
 // convenience functions for operations for beacon state collection and parsing
 // the below function is pulled from lodestar/ 
@@ -132,7 +137,7 @@ let beaconState = ssz.capella.BeaconState.deserialize(beaconStateSsz);
 let beaconStateJson = ssz.capella.BeaconState.toJson(beaconState);
 let beaconStateDeserialized = ssz.capella.BeaconState.deserializeToViewDU(beaconStateSsz);
 
-//----------------- Committees -----------------//
+//----------------- Committees / Validators -----------------//
 
 let headBlock = await getBlockApi("head");
 // get current head slot and corresponding epochs
@@ -141,32 +146,9 @@ const headSlot = headBlock.data.message.slot
 // The lodestar node only locally caches the three most recent epochs for committee data. Thus the preprocessor will fetch the head epoch and set (head epoch -1) as the target epoch, to ensure that the slots will be complete.
 const headEpoch = headBlock.data.message.body.attestations[0].data.target.epoch
 const targetEpoch = headEpoch-1;
+var targetEpochSlots = [];
 
 let pubKeyPoints: ProjPointType<bigint>[] = [];
-
-// passing epoch number precedes the slot parameter.
-// getEpochCommittees includes validator shuffling
-let committee = await getEpochCommitteeApi(headSlot, targetEpoch);
-
-for (i=0;i<committee.data.length;i++) {
-    // gets all pubkeys of validators in committee.data.validators array as a Point.
-    pubKeyPoints.push(getPubkeysForIndices(beaconStateDeserialized.validators, committee.data[i].validators).map(bytes => bls12_381.G1.ProjectivePoint.fromHex(bytesToHex(bytes))));
-}
-
-pubKeyPoints = pubKeyPoints.flat(1);
-const committeePubkeys = chunkArray(pubKeyPoints, pubKeyPoints.length);
-const aggregatedPubKeys = committeePubkeys.map((pubKeys) => bls12_381.aggregatePublicKeys(pubKeys));
-let bytesPubkeys = aggregatedPubKeys.map((aggPubkey) => Array.from(g1PointToBytesLE(aggPubkey, false)));
-
-// console.log("bytesPubkeys: ", bytesPubkeys);
-// console.log("serialize bytespubkeys: ", serialize(bytesPubkeys));
-
-fs.writeFileSync(
-    `../test_data/sepolia_aggregated_pubkeys.json`,
-    serialize(bytesPubkeys)
-);
-
-// ------------ Validators ------------ // 
 
 type Validator = ValueOf<typeof ValidatorContainer>;
 let validators: Validator[] = [];
@@ -194,8 +176,56 @@ const ValidatorContainer = new ContainerType(
 
 export const ValidatorsSsz = new ListCompositeType(ValidatorContainer, VALIDATOR_LIMIT);
 
-// loop length set to N_VALIDATORS * N_COMMITTEES
-for (let i=0; i<N_VALIDATORS * N_COMMITTEES;i++) {
+// passing epoch number precedes the slot parameter.
+// getEpochCommittees method includes validator shuffling.
+let committee = await getEpochCommitteeApi(headSlot, targetEpoch);
+
+// in the case that the provided N_COMMITTEES arg is larger than the actual committee size.
+if (N_COMMITTEES > committee.data.length) {
+    N_COMMITTEES = committee?.data.length;
+}
+for (var i=0;i<N_COMMITTEES;i++) {
+    // gets all pubkeys of validators in committee.data.validators array as Point.
+    pubKeyPoints.push(getPubkeysForIndices(beaconStateDeserialized.validators, committee.data[i].validators).map(bytes => bls12_381.G1.ProjectivePoint.fromHex(bytesToHex(bytes))));
+
+    targetEpochSlots.push(committee.data[i].slot)
+
+    // gets committee validators and pushes to array
+    for (var j=0;j<committee.data[i].validators.length;j++) {
+        let pubkey = fromHex(beaconStateJson.validators[i].pubkey);
+        const paddedPubkey = new Uint8Array(48);
+        paddedPubkey.set(pubkey, 0);
+
+        validators.push({
+            pubkey: paddedPubkey,
+            withdrawalCredentials: fromHex(beaconStateJson.validators[committee.data[i].validators[j]].withdrawal_credentials),
+            effectiveBalance: beaconStateJson.validators[committee.data[i].validators[j]].effective_balance,
+            slashed: beaconStateJson.validators[committee.data[i].validators[j]].slashed,
+            activationEligibilityEpoch: beaconStateJson.validators[committee.data[i].validators[j]].activation_eligibility_epoch,
+            activationEpoch: beaconStateJson.validators[committee.data[i].validators[j]].activation_epoch,
+            exitEpoch: beaconStateJson.validators[committee.data[i].validators[j]].exit_epoch,
+            withdrawableEpoch: beaconStateJson.validators[committee.data[i].validators[j]].withdrawable_epoch
+        });
+
+        // QUESTION: do the validators' index need to be the same in the array as that of the beacon state?
+        validatorBaseGindices.push(ValidatorsSsz.getPathInfo([]).gindex);
+        gindices.push(ValidatorsSsz.getPathInfo([j, 'pubkey']).gindex * 2n);
+        gindices.push(ValidatorsSsz.getPathInfo([j, 'pubkey']).gindex * 2n + 1n);
+        gindices.push(ValidatorsSsz.getPathInfo([j, 'effectiveBalance']).gindex);
+        gindices.push(ValidatorsSsz.getPathInfo([j, 'slashed']).gindex);
+        gindices.push(ValidatorsSsz.getPathInfo([j, 'activationEpoch']).gindex);
+        gindices.push(ValidatorsSsz.getPathInfo([j, 'exitEpoch']).gindex);
+
+        nonRlcGindices.push(ValidatorsSsz.getPathInfo([j, 'effectiveBalance']).gindex);
+        nonRlcGindices.push(ValidatorsSsz.getPathInfo([j, 'slashed']).gindex);
+        nonRlcGindices.push(ValidatorsSsz.getPathInfo([j, 'activationEpoch']).gindex);
+        nonRlcGindices.push(ValidatorsSsz.getPathInfo([j, 'exitEpoch']).gindex);
+    }
+}
+
+// can alternatively push all the validators in the beacon state into the array.
+/*
+for (let i=0; i<beaconStateJson.validators.length-1;i++) {
     let pubkey = fromHex(beaconStateJson.validators[i].pubkey);
     const paddedPubkey = new Uint8Array(48);
     paddedPubkey.set(pubkey, 0);
@@ -223,28 +253,45 @@ for (let i=0; i<N_VALIDATORS * N_COMMITTEES;i++) {
     nonRlcGindices.push(ValidatorsSsz.getPathInfo([i, 'slashed']).gindex);
     nonRlcGindices.push(ValidatorsSsz.getPathInfo([i, 'activationEpoch']).gindex);
     nonRlcGindices.push(ValidatorsSsz.getPathInfo([i, 'exitEpoch']).gindex);
+}
+*/
 
+pubKeyPoints = pubKeyPoints.flat(1);
+const committeePubkeys = chunkArray(pubKeyPoints, pubKeyPoints.length);
+const aggregatedPubKeys = committeePubkeys.map((pubKeys) => bls12_381.aggregatePublicKeys(pubKeys));
+let bytesPubkeys = aggregatedPubKeys.map((aggPubkey) => Array.from(g1PointToBytesLE(aggPubkey, false)));
+
+// console.log("bytesPubkeys: ", bytesPubkeys);
+// console.log("serialize bytespubkeys: ", serialize(bytesPubkeys));
+
+if (FILE == "all" || FILE == "pubkeys") {
+    fs.writeFileSync(
+        `../test_data/sepolia_aggregated_pubkeys.json`,
+        serialize(bytesPubkeys)
+    );
 }
 
 let view = ValidatorsSsz.toView(validators);
 
-fs.writeFileSync(
-    `../test_data/sepolia_validators.json`,
-    serialize(Array.from(validators.entries()).map(([i, v]) => ({
-        id: i,
-        shufflePos: i,
-        committee: Math.floor(i / N_VALIDATORS),
-        isActive: !v.slashed && v.activationEpoch <= targetEpoch && targetEpoch < v.exitEpoch,
-        isAttested: true,
-        pubkey: Array.from(v.pubkey),
-        pubkeyUncompressed: Array.from(g1PointToBytesLE(pubKeyPoints[i], false)),
-        effectiveBalance: v.effectiveBalance,
-        slashed: v.slashed,
-        activationEpoch: v.activationEpoch,
-        exitEpoch: v.exitEpoch,
-        gindex: validatorBaseGindices[i]
-    })))
-);
+// if (FILE == "all" || FILE == "aggregation") {
+    // fs.writeFileSync(
+    //     `../test_data/sepolia_validators.json`,
+    //     serialize(Array.from(validators.entries()).map(([i, v]) => ({
+    //         id: i,
+    //         shufflePos: i,
+    //         committee: Math.floor(i / N_VALIDATORS),
+    //         isActive: !v.slashed && v.activationEpoch <= targetEpoch && targetEpoch < v.exitEpoch,
+    //         isAttested: true,
+    //         pubkey: Array.from(v.pubkey),
+    //         pubkeyUncompressed: Array.from(g1PointToBytesLE(pubKeyPoints[i], false)),
+    //         effectiveBalance: v.effectiveBalance,
+    //         slashed: v.slashed,
+    //         activationEpoch: v.activationEpoch,
+    //         exitEpoch: v.exitEpoch,
+    //         gindex: validatorBaseGindices[i]
+    //     })))
+    // );
+// }
 
 //----------------- State tree -----------------//
 
@@ -260,10 +307,12 @@ let [partial_tree, trace] = createNodeFromMultiProofWithTrace(proof.leaves, proo
 // console.log("proof: ", proof)
 // console.log("trace: ", serialize(trace))
 
-fs.writeFileSync(
-    `../test_data/sepolia_beacon_state.json`,
-    serialize(trace)
-)
+if (FILE == "all" || FILE == "trace") {
+    fs.writeFileSync(
+        `../test_data/sepolia_merkle_trace.json`,
+        serialize(trace)
+    )
+}
 
 // ------------ Attestations ------------ //
 
@@ -271,7 +320,8 @@ type Attestations = ValueOf<typeof ssz.phase0.BeaconBlockBody.fields.attestation
 let attestations: Attestations = [];
 
 // get all blocks within a target epoch with known slot id's for corresponding epoch.
-for (var i=headSlot; ; i--) {
+/*
+for (i=headSlot; ; i--) {
     let blockAttestation = await getBlockAttestationApi(i);
     if (blockAttestation == undefined) {
         // console.log("empty slot");
@@ -304,14 +354,39 @@ for (var i=headSlot; ; i--) {
     if (blockAttestation.data[0].data.source.epoch == targetEpoch-2) {
         break;
     }
-
 };
+*/
+
+// gathers the attestation signatures from the slots in N_COMMITTEE
+for (i=0;i<targetEpochSlots.length;i++) {
+    let blockAttestation = await getBlockAttestationApi(targetEpochSlots[i]);
+
+    let data = {
+        slot: blockAttestation.data[0].data.slot,
+        index: blockAttestation.data[0].data.index,
+        beaconBlockRoot: blockAttestation.data[0].data.beaconBlockRoot,
+        source: {
+            epoch: blockAttestation.data[0].data.source.epoch,
+            root: blockAttestation.data[0].data.source.root
+        },
+        target: {
+            epoch: blockAttestation.data[0].data.target.epoch,
+            root: blockAttestation.data[0].data.target.root
+        }
+    };
+
+    attestations.push({
+        aggregationBits: blockAttestation.data[0].aggregationBits,
+        data: data,
+        signature: blockAttestation.data[0].signature
+    });
+}
 
 let attestationJson = ssz.phase0.BeaconBlockBody.fields.attestations.toJson(attestations);
 
-// console.log("attestations: ", attestationJson);
-
-fs.writeFileSync(
-    `../test_data/sepolia_attestations.json`,
-    JSON.stringify(attestationJson)
-);
+if (FILE == "all" || FILE == "attestations") {
+    fs.writeFileSync(
+        `../test_data/sepolia_attestations.json`,
+        JSON.stringify(attestationJson)
+    );
+}
